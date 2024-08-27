e3eeda10ea8fa17081e3d5ac908cc58931e08c9a
gramatchi <nik98.champion@gmail.com>
2024-08-27 17:51:31 +0200

    fix navigate

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,16 @@
 import { NavLink } from "react-router-dom";
-import css from './Navigation.module.css'; 
-import { useSelector } from "react-redux";
-import { selectIsLogged } from "../../redux/auth/selectors";
-
+import css from "./Navigation.module.css";
 
 const Navigation = () => {
-  const isLoggedIn = useSelector(selectIsLogged);  
-
   return (
     <nav className={css.nav}>
       <NavLink to="/" className={css.navLink}>
         Home
       </NavLink>
-      {isLoggedIn && (
-        <NavLink className={css.navLink} to="/contacts">
-          Contacts
-        </NavLink>
-      )}
+
+      <NavLink className={css.navLink} to="/contacts">
+        Contacts
+      </NavLink>
     </nav>
   );
 };
--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -3,9 +3,9 @@ import { Navigate } from "react-router-dom";
 import { selectIsLogged } from "../../redux/auth/selectors";
 
 const PublicRoute = ({ children }) => {
-  const isLoggedIn  = useSelector(selectIsLogged);
+  const isLoggedIn = useSelector(selectIsLogged);
 
-  return isLoggedIn ? <Navigate to={"/contacts"} /> : children;
+  return isLoggedIn ? <Navigate to={"/contacts"} replace /> : children;
 };
 
 export default PublicRoute;
